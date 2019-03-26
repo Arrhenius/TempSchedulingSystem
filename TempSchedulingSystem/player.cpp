b6e99ed4d88c37d146750c95cd46035ec4c8858c
Abel Atkinson <Jetforce2@gmail.com>
2019-03-26 13:09:57 -0500

    Added availability structures to the player space of the program

--- TempSchedulingSystem/player.cpp
+++ TempSchedulingSystem/player.cpp
@@ -1,12 +1,11 @@
 #define _CRT_SECURE_NO_WARNINGS 1
 
-
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include "help_func.hpp"
-#include "availability.hpp"
 #include "player.hpp"
+#include "team.hpp"
 
 
 Player createPlayer(const char *fullName, const char *alias, int tz)
@@ -39,4 +38,56 @@ Player createPlayer(const char *fullName, const char *alias, int tz)
 		errExit("Invalid constant provided! Terminating program.", __FILE__, __LINE__);
 
 	return p;
+}
+
+
+void playerAvailability(Team **tm, const int playerIndex)
+{
+	if (!(*tm))
+	{
+		while (1)
+		{
+			char buffer[MENU_TKN_SIZE + 1];
+			int handle;
+			system("cls");
+			printf("\t\tPlayer Availability Menu\n\n");
+
+			printf("Which day would you like to modify?\n");
+			printf("List of choices to enter is provided: \n");
+			printf("Mon\nTue\nWed\nThu\nFri\nSat\nSun\n");
+			printf("Enter a choice exactly as listed: ");
+			fgets(buffer, MENU_TKN_SIZE, stdin);
+			handleString(buffer, __FILE__, __LINE__);
+
+			if (strcmp(buffer, "Mon") == 0)
+				handle = DOW_MON;
+			else if (strcmp(buffer, "Tue") == 0)
+				handle = DOW_TUE;
+			else if (strcmp(buffer, "Wed") == 0)
+				handle = DOW_WED;
+			else if (strcmp(buffer, "Thu") == 0)
+				handle = DOW_THU;
+			else if (strcmp(buffer, "Fri") == 0)
+				handle = DOW_FRI;
+			else if (strcmp(buffer, "Sat") == 0)
+				handle = DOW_SAT;
+			else if (strcmp(buffer, "Sun") == 0)
+				handle = DOW_SUN;
+			else
+				handle = -1;
+
+			switch (handle)
+			{
+			case DOW_MON:
+			case DOW_TUE:
+			case DOW_WED:
+			case DOW_THU:
+			case DOW_FRI:
+			case DOW_SAT:
+			case DOW_SUN:
+			default:
+
+			}
+		}
+	}
 }
--- TempSchedulingSystem/player.hpp
+++ TempSchedulingSystem/player.hpp
@@ -1,8 +1,31 @@
 #ifndef PLAYER_HPP
 #define PLAYER_HPP
 
-#include "availability.hpp"
-#define AV_MXSZ 7
+#include "team.hpp"
+
+#define DOW_MON	0
+#define DOW_TUE	1
+#define DOW_WED	2
+#define DOW_THU	3
+#define DOW_FRI	4
+#define DOW_SAT	5
+#define DOW_SUN	6
+#define AV_MXSZ	7
+
+
+// NOTE: This file inclusion seems to cause problems if not placed at the top
+// Intellisense (I think that's what it is called) seems to be buggy with this
+// This is based on forum searches
+//#include "team.hpp"	
+
+typedef struct
+{
+	char dayOfWeek[4];	// Day of the week abbr.
+	int start;				// Available start time; In military time
+	int stop;				// End time; In military time
+
+} Availability;
+
 
 typedef struct
 {
@@ -12,11 +35,9 @@ typedef struct
 											// Will use a timezone constant from help_func.hpp
 	char timeZoneFull[32];			// Full timezone name
 	Availability avl[AV_MXSZ];		// Player weekly availability
-
 } Player;
 
 
 Player createPlayer(const char *fullName, const char *alias, int tz);
-
+void playerAvailability(Team **tm, int playerIndex);
 #endif
-
